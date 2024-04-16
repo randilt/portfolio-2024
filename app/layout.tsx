57a3b2796c319250c51c167aa40f8da6c552939d
RandilT <randiltharusha72@gmail.com>
2024-04-16 16:08:57 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="bg-black">
+      <body className={inter.className}>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
